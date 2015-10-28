58769f5dc27be476e119a6ba9491dc8b37be2b46
Satnam <satnamxv63@gmail.com>
2015-10-28 08:48:19 -0700

    install backbone

---
+++ app/assets/javascripts/taglee.js
@@ -0,0 +1,13 @@
+window.Taglee = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Taglee.initialize();
+});
