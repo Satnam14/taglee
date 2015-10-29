63b530d432af113f43c4b0bc043c7ae4058672d0
Satnam <satnamxv63@gmail.com>
2015-10-29 11:46:31 -0700

    fix callback

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,12 @@
 class SessionsController < ApplicationController
   def create
-    response = Instagram.get_access_token(params[:code], :redirect_uri => "http://localhost:3000/users/auth/instagram/callback")
+    callback = nil
+    if Rails.env.development?
+      callback = "http://localhost:3000/users/auth/instagram/callback"
+    else
+      callback = "https://taglee.herokuapp.com/users/auth/instagram/callback"
+    end
+    response = Instagram.get_access_token(params[:code], :redirect_uri => callback)
 
     @user = User.find_by(uid: response.user.id)
     if @user
